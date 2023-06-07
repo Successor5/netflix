1a3d5e45eb002dcbe8442a35809a4cda49b58ba4
Debbie <119403063+debby71711@users.noreply.github.com>
2023-06-07 13:00:42 +0100

    k

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Net from './Pages/Dashboard/Componenet/net';
+function App() {
+  return (
+    <div className="App">
+      <Net/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Dashboard/Componenet/net.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import '../Styles/net.css';
+function Net(){
+    
+ 
+  return (
+    <div className="netflix-logo">
+      <div className="n-animation">
+        <div className="n-bar n-bar1"></div>
+        <div className="n-bar n-bar2"></div>
+        <div className="n-bar n-bar3"></div>
+      </div>
+      <div className="container">
+      <div className="logo">
+        <span>N</span>
+        <span>e</span>
+        <span>t</span>
+        <span>f</span>
+        <span>l</span>
+        <span>i</span>
+        <span>x</span>
+      </div>
+    </div>
+    </div>
+  );
+}
+export default Net;
